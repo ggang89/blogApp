67e22a96efee8b42062d725eec838e72f9c09806
ggang89 <ggang89@gmail.com>
2024-08-13 22:59:51 +0900

    post 페이지 css

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,39 @@
 import { useState, useEffect } from "react"
 import { styled } from "styled-components";
 
-
-const Title = styled.h1`
+const Wrapper = styled.div`
+padding: 5px 10px;
+//background-color: #ddd;
+`
+const Title = styled.h2`
   text-align: center;
+  padding: 40px 0px;
 `;
+const PostBox = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: space-evenly;
+`
+const Content = styled.div`
+width: 40%;
+height: 300px;
+border:1px solid black;
+padding: 10px 20px;
+margin-bottom:50px;
+`
+const HeadLine = styled.div`
+height:100px;
+//background-color: black;
+font-size: 18px;
+font-weight: bold;
+//text-align: center;
+align-items: center;
+display: flex;
+`
+const Text = styled.div`
+padding: 0px 10px;
+font-style:italic;
+`
 export default function Post() {
   const [loading, setLoading] = useState(true);
   const [posts, setPost]=useState([])
@@ -22,19 +51,19 @@ export default function Post() {
       {loading ? (
         <Title>... 🐧 ...</Title>
       ) : (
-        <>
+        <Wrapper>
           <Title>
-            Post
+            📃 Post
           </Title>
-          <ul>
+          <PostBox>
             {posts.map((p) => (
-              <li key={p.id}>
-                <p>📍Title: {p.title}</p>
-                <div>📃body: {p.body}</div>
-              </li>
+              <Content key={p.id}>
+                <HeadLine>▫Title : "{p.title}"</HeadLine>
+                <Text>💬 {p.body}</Text>
+              </Content>
             ))}
-          </ul>
-        </>
+          </PostBox>
+        </Wrapper>
       )}
     </>
   );

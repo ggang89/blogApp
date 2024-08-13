18636d3fc2de5c648e9d137ee00560f36ecd2085
ggang89 <ggang89@gmail.com>
2024-08-13 18:35:24 +0900

    fetch로 Title 화면에 보여줌

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
+import { styled } from "styled-components";
 
+const Title = styled.h1`
+  text-align: center;
+
+`;
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [blog, setBlog] = useState("");
+
+  const getBlog = async () => {
+    const result = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const json = await result.json();
+    console.log(json);
+    setBlog(json.title);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getBlog();
+  });
   return (
-    <div>
-   
-    </div>
+    <>
+      {loading ? (
+        <Title>Loading...🐧🐧🐧</Title>
+      ) : (
+        <>
+          <Title>Title: {blog}</Title>
+        </>
+      )}
+    </>
   );
 }
 
